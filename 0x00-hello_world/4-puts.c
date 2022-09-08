c3b4bcba84026892de8a37e89ba7b9d7a5e3f67c
David <odoublek@gmail.com>
2022-09-08 16:17:02 -0700

    tte

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- * main - main block
- * Return: 0
+
+ * main - Entry point
+
+ *
+
+ * Return: 0 on success
+
  */
+
 int main(void)
+  
 {
+  
   puts("\"Programming is like building a multilingual puzzle");
+  
   return (0);
+  
 }
