06d2e8beca1fcc42579361a48f5f9a41b5175db8
David Okeke <68374819+o-double-k@users.noreply.github.com>
2022-10-31 23:41:20 +0100

    Add files via upload

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints the letters
+ * @filename: Filename
+ * @letters: Numbers of letters printed
+ * Return: Numbers of letters printed on success
+ * or 0 otherwise
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
